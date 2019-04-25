0492b0f0acde17ee9ab832c275962c2c8d7e0ad7
DeadlyKitten <stevencarr777@gmail.com>
2019-04-24 23:52:00 -0500

    Fixed Exclusion script throwing NullReferenceExceptions.

--- CustomAvatar/FirstPersonExclusion.cs
+++ CustomAvatar/FirstPersonExclusion.cs
@@ -32,14 +32,22 @@ namespace AvatarScriptPack
 
 		public void OnFirstPersonEnabledChanged(bool firstPersonEnabled)
 		{
-			if (_deadSwitch)
+			try
 			{
-				return;
+				System.Console.WriteLine("OnFirstPersonEnabledChanged - " + firstPersonEnabled);
+				if (_deadSwitch)
+				{
+					return;
+				}
+				for (var i = 0; i < Exclude.Length; i++)
+				{
+					var excludeObject = Exclude[i];
+					excludeObject.layer = AvatarLayers.OnlyInThirdPerson;
+				}
 			}
-			for (var i = 0; i < Exclude.Length; i++)
+			catch (System.Exception e)
 			{
-				var excludeObject = Exclude[i];
-				excludeObject.layer = AvatarLayers.OnlyInThirdPerson;
+				Plugin.Log(e.StackTrace);
 			}
 		}
 
