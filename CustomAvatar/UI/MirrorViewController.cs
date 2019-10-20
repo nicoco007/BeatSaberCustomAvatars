5c351d9f35173397798a46675d9f1c4300c8df59
Nicolas Gnyra <nicolas@gnyra.com>
2019-10-20 16:26:36 -0400

    Load shader asynchronously to improve performance

--- CustomAvatar/UI/MirrorViewController.cs
+++ CustomAvatar/UI/MirrorViewController.cs
@@ -12,7 +12,6 @@ namespace CustomAvatar.UI
 		private static readonly Vector3 MirrorScale = new Vector3(0.50f, 1f, 0.25f);
 
 		private GameObject mirrorPlane;
-		private Shader stereoRenderShader;
 
 		protected override void DidActivate(bool firstActivation, ActivationType activationType)
 		{
@@ -20,19 +19,7 @@ namespace CustomAvatar.UI
 
 			if (firstActivation)
 			{
-				var shadersBundle = AssetBundle.LoadFromFile("CustomAvatars/Shaders/customavatars.assetbundle");
-				stereoRenderShader = shadersBundle.LoadAsset<Shader>("Assets/Shaders/StereoRenderShader-Unlit.shader");
-
-				if (stereoRenderShader)
-				{
-					StartCoroutine(SpawnMirror());
-				}
-				else
-				{
-					Plugin.Logger.Error("Failed to load mirror shader!");
-				}
-
-				shadersBundle.Unload(false);
+				StartCoroutine(SpawnMirror());
 			}
 		}
 
@@ -45,7 +32,26 @@ namespace CustomAvatar.UI
 
 		IEnumerator SpawnMirror()
 		{
-			yield return new WaitUntil(() => Camera.main);
+			AssetBundleCreateRequest shadersBundleCreateRequest = AssetBundle.LoadFromFileAsync("CustomAvatars/Shaders/customavatars.assetbundle");
+			yield return shadersBundleCreateRequest;
+
+			if (!shadersBundleCreateRequest.isDone || shadersBundleCreateRequest.assetBundle == null)
+			{
+				Plugin.Logger.Error("Failed to load stereo mirror shader");
+				yield break;
+			}
+
+			AssetBundleRequest assetBundleRequest = shadersBundleCreateRequest.assetBundle.LoadAssetAsync<Shader>("Assets/Shaders/StereoRenderShader-Unlit.shader");
+			yield return assetBundleRequest;
+			shadersBundleCreateRequest.assetBundle.Unload(false);
+
+			if (!assetBundleRequest.isDone || assetBundleRequest.asset == null)
+			{
+				Plugin.Logger.Error("Failed to load stereo mirror shader");
+				yield break;
+			}
+
+			Shader stereoRenderShader = assetBundleRequest.asset as Shader;
 
 			mirrorPlane = GameObject.CreatePrimitive(PrimitiveType.Plane);
 			mirrorPlane.name = "Stereo Mirror";
@@ -56,16 +62,16 @@ namespace CustomAvatar.UI
 			Renderer renderer = mirrorPlane.GetComponent<Renderer>();
 			renderer.sharedMaterial = new Material(stereoRenderShader);
 
-			/*GameObject stereoCameraHead = new GameObject("Stereo Camera Head [Stereo Mirror]");
+			GameObject stereoCameraHead = new GameObject("Stereo Camera Head [Stereo Mirror]");
 			stereoCameraHead.transform.SetParent(mirrorPlane.transform, false);
 			stereoCameraHead.transform.localScale = new Vector3(1 / MirrorScale.x, 1 / MirrorScale.y, 1 / MirrorScale.z);
 
 			GameObject stereoCameraEyeObject = new GameObject("Stereo Camera Eye [Stereo Mirror]");
 			stereoCameraEyeObject.transform.SetParent(mirrorPlane.transform, false);
+
 			Camera stereoCameraEye = stereoCameraEyeObject.AddComponent<Camera>();
-			stereoCameraEye.cullingMask = 0;
-			stereoCameraEye.cullingMask |= 1 << AvatarLayers.OnlyInThirdPerson;
-			stereoCameraEye.cullingMask |= 1 << AvatarLayers.Global;
+			stereoCameraEye.enabled = false;
+			stereoCameraEye.cullingMask = (1 << AvatarLayers.OnlyInThirdPerson) | (1 << AvatarLayers.Global);
 			stereoCameraEye.clearFlags = CameraClearFlags.SolidColor;
 			stereoCameraEye.backgroundColor = new Color(0, 0, 0, 1f);
 
@@ -75,7 +81,7 @@ namespace CustomAvatar.UI
 			stereoRenderer.isMirror = true;
 			stereoRenderer.useScissor = false;
 			stereoRenderer.canvasOriginPos = mirrorPlane.transform.position + new Vector3(-10f, 0, 0);
-			stereoRenderer.canvasOriginRot = mirrorPlane.transform.rotation;*/
+			stereoRenderer.canvasOriginRot = mirrorPlane.transform.rotation;
 		}
 	}
 }
