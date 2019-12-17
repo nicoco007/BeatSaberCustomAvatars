309a4254b9b467cfa5d13f1aaf3d72e988b26ce2
Nicolas Gnyra <nicolas@gnyra.com>
2019-12-16 20:48:04 -0500

    Fix default camera near clip plane

--- Source/CustomAvatar/Utilities/Settings.cs
+++ Source/CustomAvatar/Utilities/Settings.cs
@@ -10,7 +10,7 @@ namespace CustomAvatar.Utilities
         public string previousAvatarPath { get; set; }
         public float playerArmSpan { get; set; } = 1.7f;
         public bool calibrateFullBodyTrackingOnStart { get; set; } = false;
-        public float cameraNearClipPlane { get; set; } = 0.03f;
+        public float cameraNearClipPlane { get; set; } = 0.3f;
         public FullBodyMotionSmoothing fullBodyMotionSmoothing { get; } = new FullBodyMotionSmoothing();
         public FullBodyCalibration fullBodyCalibration { get; } = new FullBodyCalibration();
 
