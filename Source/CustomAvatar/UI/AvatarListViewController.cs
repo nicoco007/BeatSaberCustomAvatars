932e7df5f6a03b11aab4591d4096e2ae71b1d89c
Nicolas Gnyra <nicolas@gnyra.com>
2020-01-26 03:03:20 -0500

    Add default icon for avatars that don't have one

--- Source/CustomAvatar/UI/AvatarListViewController.cs
+++ Source/CustomAvatar/UI/AvatarListViewController.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using BeatSaberMarkupLanguage.Attributes;
 using BeatSaberMarkupLanguage.Components;
 using BeatSaberMarkupLanguage.ViewControllers;
@@ -24,12 +26,26 @@ namespace CustomAvatar.UI
 
         private List<CustomAvatar> _avatars = new List<CustomAvatar>();
         private LevelListTableCell _tableCellTemplate;
+
+        private Texture _blankAvatarIcon;
         
         protected override void DidActivate(bool firstActivation, ActivationType type)
         {
             base.DidActivate(firstActivation, type);
 
             AvatarManager.instance.avatarChanged += OnAvatarChanged;
+            
+            Texture2D texture = new Texture2D(0, 0);
+
+            using (Stream spriteStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("CustomAvatar.Resources.mystery-man.png"))
+            {
+                byte[] rawSprite = new byte[spriteStream.Length];
+                spriteStream.Read(rawSprite, 0, (int) spriteStream.Length);
+                texture.LoadImage(rawSprite);
+            }
+
+            _blankAvatarIcon = texture;
+
 
             if (firstActivation) FirstActivation();
         }
@@ -136,7 +152,7 @@ namespace CustomAvatar.UI
 
             tableCell.GetPrivateField<LevelListTableCell, TextMeshProUGUI>("_songNameText").text = avatar.descriptor.name;
             tableCell.GetPrivateField<LevelListTableCell, TextMeshProUGUI>("_authorText").text = avatar.descriptor.author;
-            tableCell.GetPrivateField<LevelListTableCell, RawImage>("_coverRawImage").texture = avatar.descriptor.cover?.texture ?? Texture2D.blackTexture;
+            tableCell.GetPrivateField<LevelListTableCell, RawImage>("_coverRawImage").texture = avatar.descriptor.cover?.texture ?? _blankAvatarIcon;
 
             return tableCell;
         }
