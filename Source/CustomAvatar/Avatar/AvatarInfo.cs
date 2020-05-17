1feeeec25bc69590d06a2f68fe2cd0a70b8e3939
Nicolas Gnyra <nicolas@gnyra.com>
2020-05-17 12:00:55 -0400

    Fix issues with AvatarInfo

--- Source/CustomAvatar/Avatar/AvatarInfo.cs
+++ Source/CustomAvatar/Avatar/AvatarInfo.cs
@@ -4,36 +4,38 @@ using UnityEngine;
 
 namespace CustomAvatar.Avatar
 {
-    internal struct AvatarInfo
+    internal class AvatarInfo
     {
-        internal string name { get; }
-        internal string author { get; }
-        internal Texture2D icon { get; }
-        internal string fullPath { get; }
-        internal DateTime created { get; }
-        internal DateTime modified { get; }
+        internal readonly string name;
+        internal readonly string author;
+        internal readonly Texture2D icon;
+        internal readonly string fullPath;
+        internal readonly long size;
+        internal readonly DateTime created;
+        internal readonly DateTime modified;
 
         internal AvatarInfo(LoadedAvatar avatar)
         {
             name = avatar.descriptor.name;
             author = avatar.descriptor.author;
-            icon = avatar.descriptor.cover ? avatar.descriptor.cover.texture : Texture2D.blackTexture;
+            icon = avatar.descriptor.cover ? avatar.descriptor.cover.texture : null;
             fullPath = avatar.fullPath;
 
             var fileInfo = new FileInfo(fullPath);
 
+            size = fileInfo.Length;
             created = fileInfo.CreationTimeUtc;
             modified = fileInfo.LastWriteTimeUtc;
         }
 
         public static bool operator ==(AvatarInfo left, AvatarInfo right)
         {
-            return left.name == right.name && left.author == right.author && left.fullPath == right.fullPath && left.created == right.created && left.modified == right.modified;
+            return left.name == right.name && left.author == right.author && left.fullPath == right.fullPath && left.size == right.size && left.created == right.created && left.modified == right.modified;
         }
 
         public static bool operator !=(AvatarInfo left, AvatarInfo right)
         {
-            return left.name != right.name || left.author != right.author || left.fullPath != right.fullPath || left.created != right.created || left.modified != right.modified;
+            return left.name != right.name || left.author != right.author || left.fullPath != right.fullPath || left.size != right.size || left.created != right.created || left.modified != right.modified;
         }
     }
 }
