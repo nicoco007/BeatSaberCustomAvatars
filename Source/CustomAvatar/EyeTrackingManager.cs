2905068e886bb8b74fbb3b42b3b8f0fa12934b82
MoePus <547007249@qq.com>
2020-05-21 22:50:14 +0800

    Add EyeTrackingManager to support eye openness

---
+++ Source/CustomAvatar/EyeTrackingManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace CustomAvatar
+{
+    public class EyeTrackingManager : MonoBehaviour
+    {
+        public SkinnedMeshRenderer targetMesh;
+        public int leftEyeWinkleBlendShapeIndex = 0;
+        public int rightEyeWinkleBlendShapeIndex = 0;
+        public float EyeMoveStrength = 1.0f;
+
+        public bool winkleEnable = true;
+    }
+}
