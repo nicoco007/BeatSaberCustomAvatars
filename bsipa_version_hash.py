429f20e35f6232a7c494ec22dfe6f0bd81b91eb9
Nicolas Gnyra <nicolas@gnyra.com>
2020-07-17 23:29:54 -0400

    Follow semver metadata spec properly

--- bsipa_version_hash.py
+++ bsipa_version_hash.py
@@ -5,20 +5,27 @@ commit_hash = os.getenv("GIT_HASH")
 rev = os.getenv("GIT_REV")
 file_path = "Source/CustomAvatar/manifest.json"
 
-if (rev is not None and len(rev) > 0):
-    print("Skipping commit hash")
-    exit()
-
-if (commit_hash is None or len(commit_hash) == 0):
-    print("Commit hash not found in environment")
-    exit(-1)
-
 with open(file_path, "r") as json_file:
     obj = json.load(json_file)
 
-version = obj["version"]
+semver = obj["version"]
+has_metadata = semver.index("+") > 0
+version = semver[0:semver.index("+")]
 
-obj["version"] = version[0:version.rindex("-")] + "-" + commit_hash
+if (rev is not None and len(rev) > 0):
+    if rev != ("v" + version):
+        print("Git tag does not match manifest version")
+        exit(-1)
+
+    obj["version"] = version
+else:
+    if (commit_hash is None or len(commit_hash) == 0):
+        print("Commit hash not found in environment")
+        exit(-1)
+
+    obj["version"] = version + "+git." + commit_hash
+
+
 
 with open(file_path, "w") as json_file:
     json.dump(obj, json_file, indent=2)
