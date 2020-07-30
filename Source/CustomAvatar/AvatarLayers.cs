774572949acf14088dd9342e6bcea05c218d19cb
Nicolas Gnyra <nicolas@gnyra.com>
2020-07-30 02:38:43 -0400

    Use static readonly instead of const since stuff changes

--- Source/CustomAvatar/AvatarLayers.cs
+++ Source/CustomAvatar/AvatarLayers.cs
@@ -2,11 +2,11 @@ namespace CustomAvatar
 {
     public static class AvatarLayers
     {
-        public const int kAlwaysVisible = 10; // Beat Saber's "Avatar" layer
-        public const int kOnlyInThirdPerson = 20;
+        public static readonly int kAlwaysVisible = 10; // Beat Saber's "Avatar" layer
+        public static readonly int kOnlyInThirdPerson = 20;
 
-        public const int kAlwaysVisibleMask = 1 << kAlwaysVisible;
-        public const int kOnlyInThirdPersonMask = 1 << kOnlyInThirdPerson;
-        public const int kAllLayersMask = kAlwaysVisibleMask | kOnlyInThirdPersonMask;
+        public static readonly int kAlwaysVisibleMask = 1 << kAlwaysVisible;
+        public static readonly int kOnlyInThirdPersonMask = 1 << kOnlyInThirdPerson;
+        public static readonly int kAllLayersMask = kAlwaysVisibleMask | kOnlyInThirdPersonMask;
     }
 }
