b69b48cbf7707b1c79045416154fe573dc1fd4ef
Nicolas Gnyra <nicolas@gnyra.com>
2021-05-31 09:28:00 -0400

    Improve DDS header parsing

--- Source/CustomAvatar/Utilities/SimpleDdsLoader.cs
+++ Source/CustomAvatar/Utilities/SimpleDdsLoader.cs
@@ -16,6 +16,7 @@
 
 using System;
 using System.IO;
+using System.Runtime.InteropServices;
 using System.Text;
 using UnityEngine;
 
@@ -28,41 +29,60 @@ namespace CustomAvatar.Utilities
         /// </summary>
         /// <param name="textureStream">Stream that contains the DDS file.</param>
         /// <returns>A new <see cref="Texture2D"/> containing the image.</returns>
-        public static Texture2D LoadImage(Stream textureStream)
+        public static Texture2D LoadImage(Stream textureStream, bool linear = false)
         {
             // DDS header: https://docs.microsoft.com/en-us/windows/win32/direct3ddds/dds-header
             byte[] headerBytes = new byte[128];
             textureStream.Read(headerBytes, 0, headerBytes.Length);
 
-            string magic = Encoding.ASCII.GetString(headerBytes, 0, 4);
+            var handle = GCHandle.Alloc(headerBytes, GCHandleType.Pinned);
+            DdsHeader header;
 
-            if (magic != "DDS ")
+            try
+            {
+                header = Marshal.PtrToStructure<DdsHeader>(handle.AddrOfPinnedObject());
+            }
+            finally
+            {
+                handle.Free();
+            }
+
+            if (header.signature != "DDS ")
             {
                 throw new IOException("Invalid file signature");
             }
 
-            int size = BitConverter.ToInt32(headerBytes, 4);
-
-            if (size != 124)
+            if (header.size != 124)
             {
                 throw new IOException("Invalid header length");
             }
 
-            int flags = BitConverter.ToInt32(headerBytes, 8);
-
-            if ((flags & 0x1) == 0 || (flags & 0x2) == 0 || (flags & 0x4) == 0 || (flags & 0x1000) == 0)
+            if (!header.flags.HasFlag(DdsFlags.Caps) || !header.flags.HasFlag(DdsFlags.Height) || !header.flags.HasFlag(DdsFlags.Width) || !header.flags.HasFlag(DdsFlags.PixelFormat))
             {
                 throw new IOException("Invalid DDS header flags");
             }
 
-            int height = BitConverter.ToInt32(headerBytes, 12);
-            int width = BitConverter.ToInt32(headerBytes, 16);
-            string format = Encoding.ASCII.GetString(headerBytes, 84, 4);
+            if (header.ddsPixelFormat.size != 32)
+            {
+                throw new IOException("Invalid pixel format length");
+            }
 
-            bool hasMipMaps = (flags & 0x20000) != 0;
+            if (!header.ddsPixelFormat.flags.HasFlag(DdsPixelFormatFlags.FourCC))
+            {
+                throw new IOException("Only compressed textures are supported");
+            }
+
+            int mipMapCount = 0;
+
+            if (header.flags.HasFlag(DdsFlags.MipMapCount))
+            {
+                mipMapCount = header.mipMapCount;
+            }
+
+            string fourCC = header.ddsPixelFormat.fourCC;
             TextureFormat textureFormat;
 
-            switch (format)
+            switch (fourCC)
             {
                 case "DXT1":
                     textureFormat = TextureFormat.DXT1;
@@ -73,10 +93,10 @@ namespace CustomAvatar.Utilities
                     break;
 
                 default:
-                    throw new IOException($"Unexpected DDS format '{format}'");
+                    throw new IOException($"Unsupported DDS compression format '{fourCC}'");
             }
 
-            var texture = new Texture2D(width, height, textureFormat, hasMipMaps);
+            var texture = new Texture2D(header.width, header.height, textureFormat, mipMapCount, linear);
 
             byte[] textureBytes = new byte[textureStream.Length - headerBytes.Length];
             textureStream.Read(textureBytes, 0, textureBytes.Length);
@@ -85,6 +105,75 @@ namespace CustomAvatar.Utilities
             texture.Apply();
 
             return texture;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Size = 128, CharSet = CharSet.Ansi)]
+        private struct DdsHeader
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)] private byte[] _signature;
+            public int size;
+            public DdsFlags flags;
+            public int height;
+            public int width;
+            public int pitchOrLinearSize;
+            public int depth;
+            public int mipMapCount;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 11)] public int[] reserved;
+            public DdsPixelFormat ddsPixelFormat;
+            public int caps;
+            public int caps2;
+            public int caps3;
+            public int caps4;
+            public int reserved2;
+
+            public string signature
+            {
+                get => Encoding.ASCII.GetString(_signature);
+                set => _signature = Encoding.ASCII.GetBytes(value);
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Size = 32)]
+        private struct DdsPixelFormat
+        {
+            public int size;
+            public DdsPixelFormatFlags flags;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)] private byte[] _fourCC;
+            public int rgbBitCount;
+            public int rBitMask;
+            public int gBitMask;
+            public int bBitMask;
+            public int aBitMask;
+
+            public string fourCC
+            {
+                get => Encoding.ASCII.GetString(_fourCC);
+                set => _fourCC = Encoding.ASCII.GetBytes(value);
+            }
+        }
+
+        [Flags]
+        private enum DdsFlags : int
+        {
+            Caps = 0x1,
+            Height = 0x2,
+            Width = 0x4,
+            Pitch = 0x8,
+            PixelFormat = 0x1000,
+            MipMapCount = 0x20000,
+            LinearSize = 0x80000,
+            Depth = 0x800000
+        }
+
+        [Flags]
+        private enum DdsPixelFormatFlags : int
+        {
+            AlphaPixels = 0x1,
+            Alpha = 0x2,
+            FourCC = 0x4,
+            RGB = 0x40,
+            YUV = 0x200,
+            Luminance = 0x20000
         }
     }
 }
