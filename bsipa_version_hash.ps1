fcbc85dfb076e32041107cb31ef25be518a52a0c
Nicolas Gnyra <nicolas@gnyra.com>
2021-06-25 22:52:40 -0400

    Use BSMT.T to set DLL version

--- bsipa_version_hash.ps1
+++ bsipa_version_hash.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = "Stop"
 $check = [char]0x2713
 $cross = [char]0x2717
 
-if ($args.Length -ne 2) {
+if ($args.Length -ne 1) {
     Write-Host "Invalid number of arguments" -ForegroundColor Red
     exit(-1)
 }
@@ -13,36 +13,12 @@ if ($args[0] -eq "" -or !(Test-Path $args[0])) {
     exit(-1)
 }
 
-if ($args[1] -eq "" -or !(Test-Path $args[1])) {
-    Write-Host "File '$($args[1])' does not exist" -ForegroundColor Red
-    exit(-1)
-}
-
 $manifest_content = Get-Content $args[0] | ConvertFrom-Json
-$assembly_info = Get-Content $args[1]
 
 $manifest_version_str = $manifest_content.version
 $semver = [regex]::Match($manifest_version_str, '^(?<prerelease>(?<version>(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)\.(?:0|[1-9]\d*))(?:-(?:(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?)(?:\+(?:[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$')
 
-$numeric_version = $semver.groups['version'].value
 $version_with_prerelease = $semver.groups['prerelease'].value
-
-$assembly_version = [regex]::Match($assembly_info, '\[assembly: AssemblyVersion\("([0-9]+\.[0-9]+\.[0-9]+)(?:\.[0-9]+)?"\)\]').groups[1].value
-$assembly_file_version = [regex]::Match($assembly_info, '\[assembly: AssemblyFileVersion\("([0-9]+\.[0-9]+\.[0-9]+)(?:\.[0-9]+)?"\)\]').groups[1].value
-
-if ($assembly_version -ne $numeric_version) {
-    Write-Host "$cross Assembly version '$assembly_version' does not match manifest version '$numeric_version'" -ForegroundColor Red
-    exit(-1)
-} else {
-    Write-Host "$check Assembly version" -ForegroundColor Green
-}
-
-if ($assembly_file_version -ne $numeric_version) {
-    Write-Host "$cross Assembly file version '$assembly_file_version' does not match manifest version '$numeric_version'" -ForegroundColor Red
-    exit(-1)
-} else {
-    Write-Host "$check Assembly file version" -ForegroundColor Green
-}
 
 $git_hash = (git log -n 1 --pretty=%h).Trim()
 $git_tag = git tag -l --points-at HEAD
@@ -68,4 +44,4 @@ if ($git_tag -ne "" -and $git_tag.Length -gt 0) {
 Add-Content "$env:GITHUB_ENV" "ZIP_VERSION=$zip_version"
 Add-Content "$env:GITHUB_ENV" "GAME_VERSION=$($manifest_content.gameVersion)"
 
-$manifest_content | ConvertTo-Json -Compress | Set-Content $args[0]
+$manifest_content | ConvertTo-Json | Set-Content $args[0]
