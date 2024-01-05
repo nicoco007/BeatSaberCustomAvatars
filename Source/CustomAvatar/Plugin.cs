0283c4d7420e2a02f7997e4046f81928bd877724
Nicolas Gnyra <nicolas@gnyra.com>
2024-01-05 10:12:04 -0500

    Update year

--- Source/CustomAvatar/Plugin.cs
+++ Source/CustomAvatar/Plugin.cs
@@ -1,5 +1,5 @@
 //  Beat Saber Custom Avatars - Custom player models for body presence in Beat Saber.
-//  Copyright © 2018-2023  Nicolas Gnyra and Beat Saber Custom Avatars Contributors
+//  Copyright © 2018-2024  Nicolas Gnyra and Beat Saber Custom Avatars Contributors
 //
 //  This library is free software: you can redistribute it and/or
 //  modify it under the terms of the GNU Lesser General Public
