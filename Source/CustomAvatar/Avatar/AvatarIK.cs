9d61e3500ea19e0d572c392f5ec93c381ad4232e
Nicolas Gnyra <nicolas@gnyra.com>
2025-03-29 17:15:38 -0400

    Reduce copy-paste in `CopyManagerFieldsToVRIK`

--- Source/CustomAvatar/Avatar/AvatarIK.cs
+++ Source/CustomAvatar/Avatar/AvatarIK.cs
@@ -305,51 +305,54 @@ namespace CustomAvatar.Avatar
 
         private static void CopyManagerFieldsToVRIK(VRIKManager vrikManager, VRIK vrik)
         {
-            vrik.references.root = vrikManager.references_root;
-            vrik.references.pelvis = vrikManager.references_pelvis;
-            vrik.references.spine = vrikManager.references_spine;
-            vrik.references.chest = vrikManager.references_chest;
-            vrik.references.neck = vrikManager.references_neck;
-            vrik.references.head = vrikManager.references_head;
-            vrik.references.leftShoulder = vrikManager.references_leftShoulder;
-            vrik.references.leftUpperArm = vrikManager.references_leftUpperArm;
-            vrik.references.leftForearm = vrikManager.references_leftForearm;
-            vrik.references.leftHand = vrikManager.references_leftHand;
-            vrik.references.rightShoulder = vrikManager.references_rightShoulder;
-            vrik.references.rightUpperArm = vrikManager.references_rightUpperArm;
-            vrik.references.rightForearm = vrikManager.references_rightForearm;
-            vrik.references.rightHand = vrikManager.references_rightHand;
-            vrik.references.leftThigh = vrikManager.references_leftThigh;
-            vrik.references.leftCalf = vrikManager.references_leftCalf;
-            vrik.references.leftFoot = vrikManager.references_leftFoot;
-            vrik.references.leftToes = vrikManager.references_leftToes;
-            vrik.references.rightThigh = vrikManager.references_rightThigh;
-            vrik.references.rightCalf = vrikManager.references_rightCalf;
-            vrik.references.rightFoot = vrikManager.references_rightFoot;
-            vrik.references.rightToes = vrikManager.references_rightToes;
+            References references = vrik.references;
+            references.root = vrikManager.references_root;
+            references.pelvis = vrikManager.references_pelvis;
+            references.spine = vrikManager.references_spine;
+            references.chest = vrikManager.references_chest;
+            references.neck = vrikManager.references_neck;
+            references.head = vrikManager.references_head;
+            references.leftShoulder = vrikManager.references_leftShoulder;
+            references.leftUpperArm = vrikManager.references_leftUpperArm;
+            references.leftForearm = vrikManager.references_leftForearm;
+            references.leftHand = vrikManager.references_leftHand;
+            references.rightShoulder = vrikManager.references_rightShoulder;
+            references.rightUpperArm = vrikManager.references_rightUpperArm;
+            references.rightForearm = vrikManager.references_rightForearm;
+            references.rightHand = vrikManager.references_rightHand;
+            references.leftThigh = vrikManager.references_leftThigh;
+            references.leftCalf = vrikManager.references_leftCalf;
+            references.leftFoot = vrikManager.references_leftFoot;
+            references.leftToes = vrikManager.references_leftToes;
+            references.rightThigh = vrikManager.references_rightThigh;
+            references.rightCalf = vrikManager.references_rightCalf;
+            references.rightFoot = vrikManager.references_rightFoot;
+            references.rightToes = vrikManager.references_rightToes;
 
-            vrik.solver.plantFeet = vrikManager.solver_plantFeet;
-            vrik.solver.spine.headTarget = vrikManager.solver_spine_headTarget;
-            vrik.solver.spine.positionWeight = vrikManager.solver_spine_positionWeight;
-            vrik.solver.spine.rotationWeight = vrikManager.solver_spine_rotationWeight;
-            vrik.solver.spine.pelvisTarget = vrikManager.solver_spine_pelvisTarget;
-            vrik.solver.spine.pelvisPositionWeight = vrikManager.solver_spine_pelvisPositionWeight;
-            vrik.solver.spine.pelvisRotationWeight = vrikManager.solver_spine_pelvisRotationWeight;
-            vrik.solver.spine.chestGoal = vrikManager.solver_spine_chestGoal;
-            vrik.solver.spine.chestGoalWeight = vrikManager.solver_spine_chestGoalWeight;
-            vrik.solver.spine.minHeadHeight = vrikManager.solver_spine_minHeadHeight;
-            vrik.solver.spine.bodyPosStiffness = vrikManager.solver_spine_bodyPosStiffness;
-            vrik.solver.spine.bodyRotStiffness = vrikManager.solver_spine_bodyRotStiffness;
-            vrik.solver.spine.neckStiffness = vrikManager.solver_spine_neckStiffness;
-            vrik.solver.spine.rotateChestByHands = vrikManager.solver_spine_rotateChestByHands;
-            vrik.solver.spine.chestClampWeight = vrikManager.solver_spine_chestClampWeight;
-            vrik.solver.spine.headClampWeight = vrikManager.solver_spine_headClampWeight;
-            vrik.solver.spine.moveBodyBackWhenCrouching = vrikManager.solver_spine_moveBodyBackWhenCrouching;
-            vrik.solver.spine.maintainPelvisPosition = vrikManager.solver_spine_maintainPelvisPosition;
-            vrik.solver.spine.maxRootAngle = vrikManager.solver_spine_maxRootAngle;
+            IKSolverVR solver = vrik.solver;
+            solver.plantFeet = vrikManager.solver_plantFeet;
 
-            var leftArm = (CustomIKSolverVR.CustomArm)vrik.solver.leftArm;
-            leftArm.shoulderPitchOffset = CalculatePitchOffset(true, vrik.references.root, vrik.references.leftShoulder, vrik.references.leftUpperArm);
+            IKSolverVR.Spine spine = solver.spine;
+            spine.headTarget = vrikManager.solver_spine_headTarget;
+            spine.positionWeight = vrikManager.solver_spine_positionWeight;
+            spine.rotationWeight = vrikManager.solver_spine_rotationWeight;
+            spine.pelvisTarget = vrikManager.solver_spine_pelvisTarget;
+            spine.pelvisPositionWeight = vrikManager.solver_spine_pelvisPositionWeight;
+            spine.pelvisRotationWeight = vrikManager.solver_spine_pelvisRotationWeight;
+            spine.chestGoal = vrikManager.solver_spine_chestGoal;
+            spine.chestGoalWeight = vrikManager.solver_spine_chestGoalWeight;
+            spine.minHeadHeight = vrikManager.solver_spine_minHeadHeight;
+            spine.bodyPosStiffness = vrikManager.solver_spine_bodyPosStiffness;
+            spine.bodyRotStiffness = vrikManager.solver_spine_bodyRotStiffness;
+            spine.neckStiffness = vrikManager.solver_spine_neckStiffness;
+            spine.rotateChestByHands = vrikManager.solver_spine_rotateChestByHands;
+            spine.chestClampWeight = vrikManager.solver_spine_chestClampWeight;
+            spine.headClampWeight = vrikManager.solver_spine_headClampWeight;
+            spine.moveBodyBackWhenCrouching = vrikManager.solver_spine_moveBodyBackWhenCrouching;
+            spine.maintainPelvisPosition = vrikManager.solver_spine_maintainPelvisPosition;
+            spine.maxRootAngle = vrikManager.solver_spine_maxRootAngle;
+
+            IKSolverVR.Arm leftArm = solver.leftArm;
             leftArm.target = vrikManager.solver_leftArm_target;
             leftArm.bendGoal = vrikManager.solver_leftArm_bendGoal;
             leftArm.positionWeight = vrikManager.solver_leftArm_positionWeight;
@@ -364,8 +367,7 @@ namespace CustomAvatar.Avatar
             leftArm.armLengthMlp = vrikManager.solver_leftArm_armLengthMlp;
             leftArm.stretchCurve = vrikManager.solver_leftArm_stretchCurve;
 
-            var rightArm = (CustomIKSolverVR.CustomArm)vrik.solver.rightArm;
-            rightArm.shoulderPitchOffset = CalculatePitchOffset(false, vrik.references.root, vrik.references.rightShoulder, vrik.references.rightUpperArm);
+            IKSolverVR.Arm rightArm = solver.rightArm;
             rightArm.target = vrikManager.solver_rightArm_target;
             rightArm.bendGoal = vrikManager.solver_rightArm_bendGoal;
             rightArm.positionWeight = vrikManager.solver_rightArm_positionWeight;
@@ -380,58 +382,47 @@ namespace CustomAvatar.Avatar
             rightArm.armLengthMlp = vrikManager.solver_rightArm_armLengthMlp;
             rightArm.stretchCurve = vrikManager.solver_rightArm_stretchCurve;
 
-            vrik.solver.leftLeg.target = vrikManager.solver_leftLeg_target;
-            vrik.solver.leftLeg.bendGoal = vrikManager.solver_leftLeg_bendGoal;
-            vrik.solver.leftLeg.positionWeight = vrikManager.solver_leftLeg_positionWeight;
-            vrik.solver.leftLeg.rotationWeight = vrikManager.solver_leftLeg_rotationWeight;
-            vrik.solver.leftLeg.bendGoalWeight = vrikManager.solver_leftLeg_bendGoalWeight;
-            vrik.solver.leftLeg.swivelOffset = vrikManager.solver_leftLeg_swivelOffset;
-            vrik.solver.leftLeg.bendToTargetWeight = vrikManager.solver_leftLeg_bendToTargetWeight;
-            vrik.solver.leftLeg.legLengthMlp = vrikManager.solver_leftLeg_legLengthMlp;
-            vrik.solver.leftLeg.stretchCurve = vrikManager.solver_leftLeg_stretchCurve;
-            vrik.solver.rightLeg.target = vrikManager.solver_rightLeg_target;
-            vrik.solver.rightLeg.bendGoal = vrikManager.solver_rightLeg_bendGoal;
-            vrik.solver.rightLeg.positionWeight = vrikManager.solver_rightLeg_positionWeight;
-            vrik.solver.rightLeg.rotationWeight = vrikManager.solver_rightLeg_rotationWeight;
-            vrik.solver.rightLeg.bendGoalWeight = vrikManager.solver_rightLeg_bendGoalWeight;
-            vrik.solver.rightLeg.swivelOffset = vrikManager.solver_rightLeg_swivelOffset;
-            vrik.solver.rightLeg.bendToTargetWeight = vrikManager.solver_rightLeg_bendToTargetWeight;
-            vrik.solver.rightLeg.legLengthMlp = vrikManager.solver_rightLeg_legLengthMlp;
-            vrik.solver.rightLeg.stretchCurve = vrikManager.solver_rightLeg_stretchCurve;
-            vrik.solver.locomotion.weight = vrikManager.solver_locomotion_weight;
-            vrik.solver.locomotion.footDistance = vrikManager.solver_locomotion_footDistance;
-            vrik.solver.locomotion.stepThreshold = vrikManager.solver_locomotion_stepThreshold;
-            vrik.solver.locomotion.angleThreshold = vrikManager.solver_locomotion_angleThreshold;
-            vrik.solver.locomotion.comAngleMlp = vrikManager.solver_locomotion_comAngleMlp;
-            vrik.solver.locomotion.maxVelocity = vrikManager.solver_locomotion_maxVelocity;
-            vrik.solver.locomotion.velocityFactor = vrikManager.solver_locomotion_velocityFactor;
-            vrik.solver.locomotion.maxLegStretch = vrikManager.solver_locomotion_maxLegStretch;
-            vrik.solver.locomotion.rootSpeed = vrikManager.solver_locomotion_rootSpeed;
-            vrik.solver.locomotion.stepSpeed = vrikManager.solver_locomotion_stepSpeed;
-            vrik.solver.locomotion.stepHeight = vrikManager.solver_locomotion_stepHeight;
-            vrik.solver.locomotion.heelHeight = vrikManager.solver_locomotion_heelHeight;
-            vrik.solver.locomotion.relaxLegTwistMinAngle = vrikManager.solver_locomotion_relaxLegTwistMinAngle;
-            vrik.solver.locomotion.relaxLegTwistSpeed = vrikManager.solver_locomotion_relaxLegTwistSpeed;
-            vrik.solver.locomotion.stepInterpolation = vrikManager.solver_locomotion_stepInterpolation;
-            vrik.solver.locomotion.offset = vrikManager.solver_locomotion_offset;
-            vrik.solver.locomotion.onLeftFootstep = vrikManager.solver_locomotion_onLeftFootstep;
-            vrik.solver.locomotion.onRightFootstep = vrikManager.solver_locomotion_onRightFootstep;
-        }
+            IKSolverVR.Leg leftLeg = solver.leftLeg;
+            leftLeg.target = vrikManager.solver_leftLeg_target;
+            leftLeg.bendGoal = vrikManager.solver_leftLeg_bendGoal;
+            leftLeg.positionWeight = vrikManager.solver_leftLeg_positionWeight;
+            leftLeg.rotationWeight = vrikManager.solver_leftLeg_rotationWeight;
+            leftLeg.bendGoalWeight = vrikManager.solver_leftLeg_bendGoalWeight;
+            leftLeg.swivelOffset = vrikManager.solver_leftLeg_swivelOffset;
+            leftLeg.bendToTargetWeight = vrikManager.solver_leftLeg_bendToTargetWeight;
+            leftLeg.legLengthMlp = vrikManager.solver_leftLeg_legLengthMlp;
+            leftLeg.stretchCurve = vrikManager.solver_leftLeg_stretchCurve;
 
-        private static float CalculatePitchOffset(bool isLeft, Transform root, Transform shoulder, Transform upperArm)
-        {
-            if (root == null || shoulder == null || upperArm == null)
-            {
-                return -30f;
-            }
+            IKSolverVR.Leg rightLeg = solver.rightLeg;
+            rightLeg.target = vrikManager.solver_rightLeg_target;
+            rightLeg.bendGoal = vrikManager.solver_rightLeg_bendGoal;
+            rightLeg.positionWeight = vrikManager.solver_rightLeg_positionWeight;
+            rightLeg.rotationWeight = vrikManager.solver_rightLeg_rotationWeight;
+            rightLeg.bendGoalWeight = vrikManager.solver_rightLeg_bendGoalWeight;
+            rightLeg.swivelOffset = vrikManager.solver_rightLeg_swivelOffset;
+            rightLeg.bendToTargetWeight = vrikManager.solver_rightLeg_bendToTargetWeight;
+            rightLeg.legLengthMlp = vrikManager.solver_rightLeg_legLengthMlp;
+            rightLeg.stretchCurve = vrikManager.solver_rightLeg_stretchCurve;
 
-            // Reading IKSolverVR.Arm's Shoulder Pitch section, it looks like:
-            // - chestForward is essentially just the root forward
-            // - pitch is always (90 degrees to either side of chestUp) * chestRotation, so effectively left and right of the chest.
-            Vector3 forward = root.forward;
-            Vector3 right = root.right;
-
-            return Vector3.SignedAngle(isLeft ? -right : right, upperArm.position - shoulder.position, isLeft ? forward : -forward) - 45f;
+            IKSolverVR.Locomotion locomotion = solver.locomotion;
+            locomotion.weight = vrikManager.solver_locomotion_weight;
+            locomotion.footDistance = vrikManager.solver_locomotion_footDistance;
+            locomotion.stepThreshold = vrikManager.solver_locomotion_stepThreshold;
+            locomotion.angleThreshold = vrikManager.solver_locomotion_angleThreshold;
+            locomotion.comAngleMlp = vrikManager.solver_locomotion_comAngleMlp;
+            locomotion.maxVelocity = vrikManager.solver_locomotion_maxVelocity;
+            locomotion.velocityFactor = vrikManager.solver_locomotion_velocityFactor;
+            locomotion.maxLegStretch = vrikManager.solver_locomotion_maxLegStretch;
+            locomotion.rootSpeed = vrikManager.solver_locomotion_rootSpeed;
+            locomotion.stepSpeed = vrikManager.solver_locomotion_stepSpeed;
+            locomotion.stepHeight = vrikManager.solver_locomotion_stepHeight;
+            locomotion.heelHeight = vrikManager.solver_locomotion_heelHeight;
+            locomotion.relaxLegTwistMinAngle = vrikManager.solver_locomotion_relaxLegTwistMinAngle;
+            locomotion.relaxLegTwistSpeed = vrikManager.solver_locomotion_relaxLegTwistSpeed;
+            locomotion.stepInterpolation = vrikManager.solver_locomotion_stepInterpolation;
+            locomotion.offset = vrikManager.solver_locomotion_offset;
+            locomotion.onLeftFootstep = vrikManager.solver_locomotion_onLeftFootstep;
+            locomotion.onRightFootstep = vrikManager.solver_locomotion_onRightFootstep;
         }
     }
 }
